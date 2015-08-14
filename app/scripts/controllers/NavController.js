7f7125bfe5516691cd8970cfeb90ab8720e23cd7
Faraaz Nishtar <faraaznishtar@gmail.com>
2015-08-13 19:48:39 -0700

    style left nav

---
+++ app/scripts/controllers/NavController.js
@@ -0,0 +1,37 @@
+/**
+ * Created by iMuzz on 8/13/15.
+ */
+(function () {
+
+    angular.module('redditApp')
+        .controller('NavParCtrl', function ($scope, $timeout, $mdSidenav, $mdUtil, $log) {
+            $scope.toggleLeft = buildToggler('left');
+            /**
+             * Build handler to open/close a SideNav; when animation finishes
+             * report completion in console
+             */
+            function buildToggler(navID) {
+                var debounceFn =  $mdUtil.debounce(function(){
+                    $mdSidenav(navID)
+                        .toggle()
+                        .then(function () {
+                            $log.debug("toggle " + navID + " is done");
+                        });
+                },200);
+                return debounceFn;
+            }
+
+        });
+
+    angular.module('redditApp').controller('NavCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+
+        $scope.subreddits = ['/r/apple', '/r/angularjs', '/r/all', '/r/programmerhumor'];
+        $scope.close = function () {
+            $mdSidenav('left').close()
+                .then(function () {
+                    $log.debug("close LEFT is done");
+                });
+        };
+    });
+
+}());
